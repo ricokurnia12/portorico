c9626e45ebc1e369e389a67dbfe5ea3f80c3ac33
COCO <ricokurnia19@gmail.com>
2023-04-01 11:01:25 +0700

    finishing login register

--- frontend/src/Component/NavbarLogin.js
+++ frontend/src/Component/NavbarLogin.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import axios from 'axios';
+import baseUrl from '../baseUrl';
+import baseDirectory from '../baseDirectory';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const NavbarLogin = () => {
+const NavbarLogin = (props) => {
+  const navigate = useNavigate();
+  const logout = async () => {
+    try {
+      await axios.delete(`${baseUrl}/logout`);
+      navigate(`/${baseDirectory}/login`);
+    } catch (error) {}
+  };
+
   return (
     <nav
       className="navbar "
@@ -13,9 +25,9 @@ const NavbarLogin = () => {
         <a className="navbar-brand text-light" href="#">
           Your Logo
         </a>
-        <a className="navbar-brand text-light" href="#">
-          Login
-        </a>
+        <button onClick={logout} className="navbar-brand text-light">
+          logout
+        </button>
       </div>
     </nav>
   );
--- frontend/src/page/Dashboard.js
+++ frontend/src/page/Dashboard.js
@@ -2,28 +2,52 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode';
 import baseUrl from '../baseUrl';
+import baseDirectory from '../baseDirectory';
 import { useNavigate } from 'react-router-dom';
+import NavbarLogin from '../Component/NavbarLogin';
 
 const Dashboard = () => {
   const [name, setName] = useState('');
   const [token, setToken] = useState('');
+  const [expire, setExpire] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     refreshToken();
   }, []);
 
+  const getUsers = async () => {
+    const response = await axios.get(`${baseUrl}/users`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data);
+  };
+
   const refreshToken = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/token`);
+      const response = await axios.get(`${baseUrl}/token`);
       setToken(response.data.accessToken);
       const decoded = jwtDecode(response.data.accessToken);
       console.log('oioi', decoded);
       setName(decoded.name);
     } catch (error) {
       console.log('oioio', error.data);
+      if (error.response) {
+        navigate(`/${baseDirectory}/login`);
+      }
     }
   };
-  return <div>Halo : {name}</div>;
+  return (
+    <>
+      <NavbarLogin name={name} />
+      <div>Halo : {name}</div>;
+      <button onClick={getUsers} className="btn btn-primary mt-5">
+        get users
+      </button>
+    </>
+  );
 };
 
 export default Dashboard;
